501341501dea67c4b76a69325a0421f67b2ac0c5
shuishen49 <shuishen49@hotmail.com>
2022-09-24 14:50:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,5 +11,4 @@ sudo apt-get update \
   libxcb-xkb1 -y \
   libxkbcommon-x11-0 \
  && sudo rm -rf /var/lib/apt/lists/* \
-cd /workspace/SuperMarioBros-AI
 python3 smb_ai.py --load-file "Example world1-1"  --load-inds 1214 
